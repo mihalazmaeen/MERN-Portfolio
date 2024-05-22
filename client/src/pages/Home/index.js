4edad8b4bea9fa47d7e669f62bbc572444074207
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-05-22 11:37:34 +0600

    section title created

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header";
 import Intro from "./intro";
+import About from "./About";
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <Header />
       <div className="bg-primary px-40">
         <Intro />
+        <About/>
       </div>
     </div>
   );
