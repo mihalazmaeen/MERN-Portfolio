0bde166ad29a06d0f850e7bcbe2a0d0d89506455
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-05-22 13:04:51 +0600

    skill section done

--- client/src/pages/Home/About.js
+++ client/src/pages/Home/About.js
@@ -3,6 +3,9 @@ import SectionTitle from "../../components/SectionTitle";
 import mihalImage from "./mihal.png";
 
 function About() {
+    const skills=[
+        "PHP","JavaScript","Laravel","VueJs","ReactJs","NodeJs","SQL","HTML/CSS","JQuery/AJAX"
+    ]
   return (
     <div>
       <SectionTitle title="About" />
@@ -23,6 +26,18 @@ function About() {
             allow me to adopt new technology trends and where I can utilize my
             knowledge and skills for the organization’s growth.
           </p>
+        </div>
+      </div>
+      <div className="py-5">
+        <h1 className="text-tertiary text-xl">
+          Here are the Technologies I have used
+        </h1>
+        <div className="flex flex-wrap gap-10 mt-5">
+          {skills.map((skill) => (
+            <div className="border border-white py-3 px-5">
+              <h1 className="text-tertiary">{skill}</h1>
+            </div>
+          ))}
         </div>
       </div>
     </div>
