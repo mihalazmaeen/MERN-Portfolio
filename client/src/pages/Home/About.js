e80c86bdbe9f806c74b747a1fbe9d0b0254149e7
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-05-22 12:42:37 +0600

    about section completed

--- client/src/pages/Home/About.js
+++ client/src/pages/Home/About.js
@@ -1,10 +1,30 @@
 import React from "react";
 import SectionTitle from "../../components/SectionTitle";
+import mihalImage from "./mihal.png";
 
 function About() {
   return (
     <div>
       <SectionTitle title="About" />
+      <div className="flex w-full items-center">
+        <div className="h-[50vh] w-1/2">
+          <img
+            src={mihalImage}
+            alt="mihal"
+            height={400}
+            width={300}
+            background="transparent"
+          />
+        </div>
+        <div className="flex flex-col gap-5 w-1/2">
+          <p className="text-white">
+            Highly motivated technology enthusiast, aspire to work with cutting
+            edge innovations,looking for a role in an IT organization that will
+            allow me to adopt new technology trends and where I can utilize my
+            knowledge and skills for the organization’s growth.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
