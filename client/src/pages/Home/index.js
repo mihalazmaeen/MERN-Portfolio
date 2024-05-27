92f45f5b243344a5c2e411b60947edeaad73fda6
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-05-27 16:38:16 +0600

    left sider done

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -7,6 +7,7 @@ import Projects from "./Projects";
 import Academics from "./Academics";
 import Contact from "./Contact";
 import Footer from "./Footer";
+import LeftSider from "./LeftSider";
 
 function Home() {
   return (
@@ -20,6 +21,7 @@ function Home() {
         <Academics/>
         <Contact/>
         <Footer/>
+        <LeftSider/>
       </div>
     </div>
   );
