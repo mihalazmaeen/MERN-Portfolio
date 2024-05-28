e69bc96ca3ac80b62334af4697d0cbeec50cfb6b
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-05-28 16:08:35 +0600

    loader completed, static part done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home";
+import Loader from "./components/Loader";
 function App() {
+  const [showLoading, setShowLoading] = useState(false);
   return (
     <BrowserRouter>
+    {showLoading ? <Loader/> : null}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
