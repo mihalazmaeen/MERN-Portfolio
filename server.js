a1a016b7ae8a6b4819804ec8070dcd1468617f17
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-05-29 11:44:47 +0600

    server setup

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const app=express();
-const port=process.env.port || 5000;
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`);
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const dbConfig = require("./config/dbConfig");
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
