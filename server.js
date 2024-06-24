b5fa1eba4c2ef4c117b7bca4fd55eb90fe39babb
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-06-24 11:06:53 +0600

    server js for heroku

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const dbConfig = require("./config/dbConfig");
 const portfolioRoute = require("./routes/portfolioRoute");
 app.use(express.json());
 const path = require("path");
+if(process.env.NODE_ENV !== "production"){
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 app.use("/api/portfolio", portfolioRoute);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
